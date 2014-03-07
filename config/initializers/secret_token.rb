74927e5d31850553bbc4a535a12b498d8da9e33f
Dan Heslinga <drdan@heslingamd.com>
2014-03-06 18:47:48 -1000

    Initialize Repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PopMan::Application.config.secret_key_base = '152fcf010a46686890f2b8e6aa1d4b8c00aacc669afc22cc1da6311cac8891523506f68d64bc28a78635b6b4d648617e4c20c8eb490da11fba1f9e2a95de3fe1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PopMan::Application.config.session_store :cookie_store, key: '_pop_man_session'
